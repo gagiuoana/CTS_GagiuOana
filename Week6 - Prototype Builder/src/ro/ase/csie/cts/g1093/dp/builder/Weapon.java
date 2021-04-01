b210426a7ed6d5923678a0df37550d1902f98a83
Lenovo <Lenovo@92.55.153.145>
2021-04-01 14:36:56 +0300

    Break

---
+++ Week6 - Prototype Builder/src/ro/ase/csie/cts/g1093/dp/builder/Weapon.java
@@ -0,0 +1,12 @@
+package ro.ase.csie.cts.g1093.dp.builder;
+
+public class Weapon implements WeaponInterface{
+
+	public void pewPew() {
+		// TODO Auto-generated method stub
+		System.out.println("-> -> -> -> -> pew pew");
+	}
+	
+	
+
+}
